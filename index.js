76f5cd49e13ab5654ca6f1ef4d3f0893eecbf6a6
elpatron <aidanseantucker@gmail.com>
2023-05-24 15:05:27 -0400

    Passing both tests

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record) {
+const winningRecord = record.find(obj => obj.result === "W");
+if (winningRecord) {
+    return winningRecord.year;
+}
+else {
+    return undefined;
+}
+}
+
